1ec7c61bc5f0a0fe76ffc95f21060581e55251d5
Daniel Nuriyev <daniel.nuriyev@gmail.com>
2011-09-17 23:32:43 -0400

    fix in timeUtil

--- timeUtil.js
+++ timeUtil.js
@@ -16,25 +16,25 @@ groundjs.TimeUtil = {
         
         var re = new RegExp('[0-9]+');
         var match = re.exec(t);
-        var t = 1;
+        var v = 1;
         if(match && match.length > 0){
-            t = match[0];
+            v = match[0];
         }
 
         if(t.indexOf('second') > -1){
-            return t * 1000;
+            return v * 1000;
         } else if(t.indexOf('minute') > -1){
-            return t * 60 * 1000;
+            return v * 60 * 1000;
         } else if(t.indexOf('hour') > -1){
-            return 60 * 60 * 1000;
+            return v * 60 * 60 * 1000;
         } else if(t.indexOf('day') > -1){
-            return t * 24 * 60 * 60 * 1000;
+            return v * 24 * 60 * 60 * 1000;
         } else if(t.indexOf('week') > -1){
-            return t * 7 * 24 * 60 * 60 * 1000;
+            return v * 7 * 24 * 60 * 60 * 1000;
         } else if(t.indexOf('month') > -1){
-            return t * 31 * 24 * 60 * 60 * 1000;
+            return v * 31 * 24 * 60 * 60 * 1000;
         } else if(t.indexOf('year') > -1){
-            return t * 366 * 24 * 60 * 60 * 1000;
+            return v * 366 * 24 * 60 * 60 * 1000;
         } else return null;
     }
 }
