95645d6431ac1e87f2be640ef49c875811c57c0a
Daniel Nuriyev <daniel.nuriyev@gmail.com>
2011-09-17 21:41:59 -0400

    added seconds to timeUtil

--- timeUtil.js
+++ timeUtil.js
@@ -7,60 +7,33 @@ groundjs.TimeUtil = {
      * and returns this timespan in milliseconds.
      * Supports: minutes, hours, days, weeks, months, years.
      * 
-     * TODO: add seconds, centuries, millenia
+     * TODO: centuries, millenia
      */
     parseTimeSpan: function(t){
         
         if(t == null) return null;
         if(typeof t !== 'string') throw 'Invalid argument'
         
-        if(t.indexOf('minute') > -1){
-            var re = new RegExp('[0-9]+');
-            var match = re.exec(t);
-            var t = 1;
-            if(match && match.length > 0){
-                t = match[0]
-            }
+        var re = new RegExp('[0-9]+');
+        var match = re.exec(t);
+        var t = 1;
+        if(match && match.length > 0){
+            t = match[0];
+        }
+
+        if(t.indexOf('second') > -1){
+            return t * 1000;
+        } else if(t.indexOf('minute') > -1){
             return t * 60 * 1000;
         } else if(t.indexOf('hour') > -1){
-            var re = new RegExp('[0-9]+');
-            var match = re.exec(t);
-            var t = 1;
-            if(match && match.length > 0){
-                t = match[0]
-            }
             return 60 * 60 * 1000;
         } else if(t.indexOf('day') > -1){
-            var re = new RegExp('[0-9]+');
-            var match = re.exec(t);
-            var t = 1;
-            if(match && match.length > 0){
-                t = match[0]
-            }
             return t * 24 * 60 * 60 * 1000;
         } else if(t.indexOf('week') > -1){
-            var re = new RegExp('[0-9]+');
-            var match = re.exec(t);
-            var t = 1;
-            if(match && match.length > 0){
-                t = match[0]
-            }
             return t * 7 * 24 * 60 * 60 * 1000;
         } else if(t.indexOf('month') > -1){
-            var re = new RegExp('[0-9]+');
-            var match = re.exec(t);
-            var t = 1;
-            if(match && match.length > 0){
-                t = match[0]
-            }
             return t * 31 * 24 * 60 * 60 * 1000;
-         } else if(t.indexOf('year') > -1){
-            var re = new RegExp('[0-9]+');
-            var match = re.exec(t);
-            var t = 1;
-            if(match && match.length > 0){
-                t = match[0]
-            }
+        } else if(t.indexOf('year') > -1){
             return t * 366 * 24 * 60 * 60 * 1000;
         } else return null;
     }
