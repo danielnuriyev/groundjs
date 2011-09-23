c98a182f25cb5e7a39f51457e75714f3bf912c1a
Daniel Nuriyev <daniel.nuriyev@gmail.com>
2011-09-23 15:14:07 -0400

    added url parser, string.pad

--- prototype.js
+++ prototype.js
@@ -14,4 +14,53 @@ if(typeof String.endsWith === 'undefined'){
     };
 };
 
+/**
+ * Pads a string with the specified character up to the specitied length on left or right
+ * @param s string to pad
+ * @param pad string used for padding
+ * @param max maximum length of the resulting string
+ * @side 'left' to pad on the left (default), 'right' to pad on the right
+ */
+if(typeof String.pad === 'undefined'){
+    String.prototype.pad = function(s, pad, max, side){
+        if(side == null){
+            side = 'left';
+        } 
+        if(side != 'left' && side != 'right'){
+            throw 'side must equal "left" or "right"'
+        }
+        if(typeof s !== 'string'){
+            s = s.toString();
+        }
+        if(typeof pad !== 'string'){
+            pad = pad.toString();
+        }
+        if(typeof max !== 'number' || max < s.length()){
+            throw 'max must be a number';
+        }
+        if(max < s.length()){
+            return s;
+        }
+        var charsToPad = max - s.length();
+        var result = s;
+        if(side == 'left'){
+            while(result.length() < max){
+                result = pad + result;
+            }
+            if(result.length() > max){
+                var start = result.length() - max;
+                result = result.substring(start, result.length());
+            }
+        } else if(side == 'right'){
+            while(result.length() < max){
+                result = result + pad;
+            }
+            if(result.length() > max){
+                result = result.substring(0, max);
+            }
+        }
+        return result;
+    };
+};
+
 groundjs = {};
