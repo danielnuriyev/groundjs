794acfe7f626be5374ecffb6d6f648d992b7f99c
unknown <dnuriyev@DNURIYEV-PC.hq.netapp.com>
2014-02-18 16:31:45 -0500

    adding 'is' functions to groundjs.Type

--- core.js
+++ core.js
@@ -10,15 +10,15 @@ groundjs.Ground = {}
  */
 groundjs.Type = function(){
    
+    //DEPRECATED: use isUndefined etc. functions instead
     var UNDEFINED = 'undefined'
 	var BOOLEAN = 'boolean'
 	var NUMBER = 'number'
 	var STRING = 'string'
 	var FUNCTION = 'function'
 	var OBJECT = 'object'
-	var XML = 'xml'
-	    
-    var argsClass = '[object Arguments]',
+	
+	var argsClass = '[object Arguments]',
       arrayClass = '[object Array]',
       boolClass = '[object Boolean]',
       dateClass = '[object Date]',
@@ -27,44 +27,78 @@ groundjs.Type = function(){
       objectClass = '[object Object]',
       regexpClass = '[object RegExp]',
       stringClass = '[object String]';
-   
-    var isArguments = function(value) {
-      return value && 
-      			typeof value == OBJECT && 
-      			typeof value.length == NUMBER &&
-        		toString.call(value) == argsClass || false
-    }
-    
-    var isArray = function(value){
-        return value && 
-        		typeof value == OBJECT && 
-        		typeof value.length == NUMBER &&
-        		toString.call(value) == arrayClass || false;        
-    }
-    
-    var isBoolean = function(value) {
-      return value === true || value === false ||
-        value && typeof value == OBJECT && toString.call(value) == boolClass || false;
-    }
-    
-    var isDate = function(value) {
-      return value && typeof value == OBJECT' && toString.call(value) == dateClass || false;
-    }
-    
-    //TO BE CONTINUED
-    
+  
+  	var isUndefined = function(value) {
+  		return typeof value === UNDEFINED
+  	}
+  	
+  	var isNull = function(value) {
+  		return value === null
+  	}
+  	
+  	var isBoolean = function(value) {
+  		return typeof value == BOOLEAN
+  	}
+  	
+  	var isNumber = function(value) {
+  		return typeof value == NUMBER
+  	} 
+  	
+  	var isString = function(value) {
+  		return typeof value == STRING
+  	}
+  	
+	var isFunction = function(value) {
+		return typeof value == FUNCTION
+	}
+	
+	var isObject = function(value) {
+		return typeof value = OBJECT
+	}
+	
+	var isNaN = function(value) {
+		return isNumber(value) && value !== NaN
+	}
+	
+	var isInfinity = function(value) {
+		return value === -Infinity || value === Infinity
+	}
+	
+	var isNegativeZero = function(value) {
+		return value === 0 && 1/value === -Infinity
+	}
+	
+	var isPositiveZero = function(value) {
+		return value === 0 && 1/value === Infinity
+	}
+	
+	var isArguments = function(value) {
+		return typeof value == OBJECT && toString.call(value) == argsClass
+	}
+  	    
     return {
+    	
+    	//DEPRECATED: use isUndefined etc. functions instead
 	    UNDEFINED: UNDEFINED,
 	    BOOLEAN: BOOLEAN,
 	    NUMBER: NUMBER,
 	    STRING: STRING,
 	    FUNCTION: FUNCTION,
 	    OBJECT: OBJECT,
-	    XML: XML,
 	    
-	    isArguments: isArguments,
-	    isArray:     isArray,
-	    isBoolean:   isBoolean
+	    isUndefined		:isUndefined,
+	    isNull			:isNull,
+	    isBoolean		:isBoolean,
+	    isNumber		:isNumber,
+	    isString		:isString,
+	    isFunction		:isFunction,
+	    isObject		:isObject,
+	    isNaN			:isNaN,
+	    isInfinity		:isInfinity,
+	    isNegativeZero	:isNegativeZero,
+	    isPositiveZero	:isPositiveZero,
+	    isArguments		:isArguments  
+	    
     }
     
 }();
